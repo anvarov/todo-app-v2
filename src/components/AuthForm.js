045edd2128bd7edc776a92e4973a2d84599b8017
Akmal <akmalanvaroff@gmail.com>
2021-07-16 10:13:35 -0400

    delete amplify

--- src/components/AuthForm.js
+++ src/components/AuthForm.js
@@ -1,33 +1,62 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Auth } from 'aws-amplify';
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
+import useAsync from '../hooks/useAsync';
 
+async function signUp() {
+}
 function AuthForm({ variant }) {
+	
+	const [password, setPassword] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
+  const [username, setUsername] = useState('');
+  const { setData, setError, error, status, data, run, reset } = useAsync();
+  
+	function handleSignUp(e) {
+    e.preventDefault();
+		run(Auth.signUp(username, password))
+		console.log(data)
+    console.log(username, password);
+  }
+
+
+
   return (
     <Form>
       <Form.Group className="mb-3" controlId="formEmail">
-        <Form.Label>Email address</Form.Label>
-        <Form.Control type="email" placeholder="Enter email" />
-        <Form.Text className="text-muted">
-          We'll never share your email with anyone else.
-        </Form.Text>
+        <Form.Label>Username</Form.Label>
+        <Form.Control
+          type="text"
+          placeholder="Username"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+        />
       </Form.Group>
 
       <Form.Group className="mb-3" controlId="formPassword">
         <Form.Label>Password</Form.Label>
-        <Form.Control type="password" placeholder="Password" />
+        <Form.Control
+          type="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          placeholder="Password"
+        />
       </Form.Group>
-      <Form.Group className="mb-3" controlId={variant === 'login' ? 'formRememberMe' : 'confirmPassword'}>
+      <Form.Group
+        className="mb-3"
+        controlId={variant === 'login' ? 'formRememberMe' : 'confirmPassword'}
+      >
         {variant === 'login' ? (
           <Form.Check type="checkbox" label="Remember me" />
         ) : (
           <>
             <Form.Label>Confirm Password</Form.Label>
-            <Form.Control type="password" placeholder="Confirm Password" />
+            <Form.Control type="password" value={confirmPassword} onChange={e => setConfirmPassword(e.target.value)}placeholder="Confirm Password" />
           </>
         )}
       </Form.Group>
-      <Button variant="primary" type="submit">
+      <Button onClick={(e) => handleSignUp(e)} variant="primary" type="submit">
         Submit
       </Button>
     </Form>
--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import ReactDOM from 'react-dom'
 import Container from 'react-bootstrap/Container'
 import Amplify from "aws-amplify";
 import awsExports from "./aws-exports";
-// import Login from './screens/Login'
+import Login from './screens/Login'
 import Dashboard from './screens/Dashboard';
 import 'bootstrap/dist/css/bootstrap.min.css';
 Amplify.configure(awsExports);
@@ -10,7 +10,8 @@ Amplify.configure(awsExports);
 
 const App = (
     <Container fluid='sm'>
-        <Dashboard />
+        {/* <Dashboard /> */}
+        <Login />
     </Container>
 )
 
